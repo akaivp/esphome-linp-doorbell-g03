6e060b78849f7081565d99f0cc93234c01d8edd4
akaivp <38605049+akaivp@users.noreply.github.com>
2026-05-25 16:06:25 +0300

    fix

--- components/linp_doorbell/linp_doorbell.cpp
+++ components/linp_doorbell/linp_doorbell.cpp
@@ -22,7 +22,7 @@ void LinpDoorbellComponent::setup() {
     .parity = UART_PARITY_DISABLE,
     .stop_bits = UART_STOP_BITS_1,
     .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-    .source_clk = UART_SCLK_APB,
+    .source_clk = UART_SCLK_APB
   };
   // Устанавливаем UART2, размер буфера для RX/TX
   uart_param_config(UART_NUM_2, &uart_config);
